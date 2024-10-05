03a0f132bc9c1fd4477a0c4f517c1e11051a0abb
Ekanisu <rickard_sundvall@hotmail.com>
2024-10-05 13:52:26 +0200

    added main

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/c/workspace/github.com/Ekanisu/asteroids/venv"
+set -gx VIRTUAL_ENV "/mnt/c/workspace/github.com/Ekanisu/Asteroids/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
