fe6764083c267bff6bb44d27ec59f2ae372d43d6
Ekanisu <rickard_sundvall@hotmail.com>
2024-10-05 14:13:02 +0200

    1

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
 from constants import * 
 def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    while pygame.get_init == True:
+        fill()
+        display.flip()
+
    
 if __name__ == "__main__":
     main()
