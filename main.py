ed8b150c52a4f856c9827458c68232fc04ce7f5f
Ekanisu <rickard_sundvall@hotmail.com>
2024-10-06 08:17:33 +0200

    trekant på svart bakgrund

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import * 
+from player import * 
 def main():
     pygame.init
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(0)
-        pygame.display.flip()
+
         dt = clock.tick(60) /1000
+
+        player.draw(screen)
+
+        pygame.display.flip()
 
    
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
+
 
 class Player(CircleShape):
     def __init__(self,x,y):
@@ -8,9 +10,13 @@ class Player(CircleShape):
         self.rotation = 0
 
     def triangle(self):
-    forward = pygame.Vector2(0, 1).rotate(self.rotation)
-    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-    a = self.position + forward * self.radius
-    b = self.position - forward * self.radius - right
-    c = self.position - forward * self.radius + right
-    return [a, b, c]
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        white = (255, 255, 255)  # Define the white color
+        pygame.draw.polygon(screen, white, self.triangle(), 2)
